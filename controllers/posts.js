2606ec39598c3d6785e763cd5d7fcb95c54c553c
Pulkit3234 <pulkitsh08@gmail.com>
2021-05-11 13:29:10 +0530

    deployment

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,10 +4,10 @@ const { post } = require('../routes/posts');
 
 exports.getPosts = async (req, res) => {
 	try {
-		const postMessages = await PostMessage.find()
-			
+		const postMessages = await PostMessage.find({});
+		console.log(postMessages);
 
-		res.status(200).json({ postMessages, count });
+		res.status(200).json({ postMessages });
 	} catch (error) {
 		res.status(404).json({ message: error.message });
 	}
@@ -106,4 +106,3 @@ exports.likePost = async (req, res, next) => {
 		res.status(502).json(error);
 	}
 };
-
